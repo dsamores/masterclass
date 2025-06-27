340ab0f46c64e5bddba36ac76a392382aacb2b7d
David Amores <davidamores@gmail.com>
2025-06-28 09:43:10 +1000

    Cuenta de caracteres

--- analisis.py
+++ analisis.py
@@ -3,3 +3,9 @@ print("Hola! Soy un robot analizador de texto. Como te llamas?")
 nombre = input()
 
 print(f"Mucho gusto {nombre}!")
+
+print("Ingresa una frase para analizar:")
+
+frase = input()
+
+print(f"Esta frase tiene {len(frase)} caracteres")
