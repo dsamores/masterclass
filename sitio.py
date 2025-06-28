bb4ce33c1fb7d1e51497aa3c8c6b63099db4cdbe
David Amores <davidamores@gmail.com>
2025-06-28 10:44:10 +1000

    Anadidas pruebas

--- sitio.py
+++ sitio.py
@@ -2,14 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+def contar_vocales(texto):
+    vocales = 0
+    for i in texto.lower():
+        if i in "aeiou":
+            vocales += 1
+    return vocales
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         texto = request.form['texto']
-        vocales = 0
-        for i in texto.lower():
-            if i in "aeiou":
-                vocales += 1
+        vocales = contar_vocales(texto)
         return render_template('index.html', vocales=vocales)
     return render_template('index.html')
 
